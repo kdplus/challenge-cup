35eb4aabce56367643fc64a369933d6ac7a483a6
kdplus <15058617922@163.com>
2017-03-23 20:41:32 +0800

    set templates and static config right

--- cup/settings.py
+++ cup/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'vecSearch.apps.VecsearchConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- vecSearch/views.py
+++ vecSearch/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'page/index.html')
